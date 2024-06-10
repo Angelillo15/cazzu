5bef9a87c7a6b9f0d610de6cab3811047fe83bd6
Angelillo15 <contact@angelillo15.es>
2024-06-10 23:29:00 +0200

    Add new index structure

---
+++ nav.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var activator = document.querySelector('#nav-active');
+  var nav = document.querySelector('#nav');
+  var close = document.querySelector('#close');
+
+  activator.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
+
+  close.addEventListener('click', () => {
+     nav.classList.toggle('active');
+  });
+});
