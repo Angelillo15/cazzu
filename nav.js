27223bd2f0d76cda8d8adf9d62c7a7557ea91965
Angelillo15 <contact@angelillo15.es>
2024-06-13 09:11:15 +0200

    Start biografia and convert the nav into a partial

--- nav.js
+++ nav.js
@@ -1,19 +1,47 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var activator = document.querySelector('#nav-active');
-  var nav = document.querySelector('#nav');
-  var close = document.querySelector('#close');
-
-  activator.addEventListener('click', () => {
-    nav.classList.toggle('active');
-  });
-
-  close.addEventListener('click', () => {
-     nav.classList.toggle('active');
-  });
-});
-
 const $ = (query) => {
   return document.querySelector(query);
 };
 
-console.log($('#nav'));
+const partial_nav = `
+      <ul class="navbar flex flex-wrap bg-stone-900 justify-center p-2 rounded-xl gap-x-16 gap-y-4 p-4" id="nav">
+        <li class="navitem">
+          <a class="text-3xl" href="index.html"><b> Inicio </b></a>
+        </li>
+        <li class="navitem">
+          <a class="text-3xl" href="biografia.html"><b> Biografia </b></a>
+        </li>
+        <li class="navitem">
+          <a class="text-3xl" href="index.html"><b> Discografia </b></a>
+        </li>
+        <li class="navitem">
+          <a class="text-3xl" href="index.html"><b> Hits globales </b></a>
+        </li>
+        <li class="navitem">
+          <a class="text-3xl" href="index.html"><b> Movistar Arena </b></a>
+        </li>
+        <li class="navitem">
+          <a class="text-3xl" href="index.html"><b> Premios </b></a>
+        </li>
+        <hr>
+        <li>
+          <button class="text-3xl text-[#D080EC] hover:text-white navitem font-bold" id="close">
+            Cerrar
+          </button>
+        </li>
+      </ul>
+`
+
+document.addEventListener('DOMContentLoaded', function() {
+  const nav = $('#nav');
+  nav.innerHTML = partial_nav;
+  const activator = $('#nav-active');
+  const close = $('#close')
+  
+  close.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });  
+
+  activator.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
+});
