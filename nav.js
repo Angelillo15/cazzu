93ba11c3e625d7716a52614276ad67866b182374
Angelillo15 <contact@angelillo15.es>
2024-06-22 18:41:11 +0200

    Implements navigation and footer functionality

--- nav.js
+++ nav.js
@@ -2,6 +2,15 @@ const $ = (query) => {
   return document.querySelector(query);
 };
 
+const pages = [
+  "index.html",
+  "biografia.html",
+  "discografia.html",
+  "hits.html",
+  "movistar-arena.html",
+  "premios.html",
+];
+
 const partial_nav = `
       <ul class="navbar flex flex-wrap bg-stone-900 justify-center p-2 rounded-xl gap-x-16 gap-y-4 p-4" id="nav">
         <li class="navitem">
@@ -14,7 +23,7 @@ const partial_nav = `
           <a class="text-3xl" href="discografia.html"><b> Discografia </b></a>
         </li>
         <li class="navitem">
-          <a class="text-3xl" href="index.html"><b> Hits globales </b></a>
+          <a class="text-3xl" href="hits.html"><b> Hits globales </b></a>
         </li>
         <li class="navitem">
           <a class="text-3xl" href="index.html"><b> Movistar Arena </b></a>
@@ -31,9 +40,63 @@ const partial_nav = `
       </ul>
 `;
 
+const getNextPageWithCurrent = () => {
+  var currentPage = window.location.pathname;
+  currentPage = currentPage.split("/").pop();
+  return getNextPage(currentPage);
+}
+
+const getPreviousPageWithCurrent = () => {
+  var currentPage = window.location.pathname;
+  currentPage = currentPage.split("/").pop();
+
+  return getPreviousPage(currentPage);
+}
+
+const getNextPage = (currentPage) => {
+  const currentPageIndex = pages.indexOf(currentPage);
+  const nextPageIndex = currentPageIndex + 1;
+
+  if (nextPageIndex >= pages.length) {
+    return pages[0];
+  }
+
+  return pages[nextPageIndex];
+};
+
+const getPreviousPage = (currentPage) => {
+  const currentPageIndex = pages.indexOf(currentPage);
+  const previousPageIndex = currentPageIndex - 1;
+
+  if (previousPageIndex < 0) {
+    return pages[pages.length - 1];
+  }
+
+  return pages[previousPageIndex];
+}
+
+const partial_footer = `
+  <hr>
+  <div class="text-4xl self-center my-4 text-[#D080EC]">
+    <a href="${getPreviousPageWithCurrent()}">
+      <i class="fa-solid fa-arrow-left"></i>
+    </a>
+    <a href="index.html">
+      <i class="fa-solid fa-house"></i>
+    </a>
+    <a href="${getNextPageWithCurrent()}">
+      <i class="fa-solid fa-arrow-right"></i>
+    </a>
+  </div>
+`;
+
 document.addEventListener("DOMContentLoaded", function () {
   const nav = $("#nav");
   nav.innerHTML = partial_nav;
+
+  const footer = $("#footer");
+  footer.innerHTML = partial_footer;
+
   const activators = document.querySelectorAll("#nav-active");
   const closers = document.querySelectorAll("#close");
 
