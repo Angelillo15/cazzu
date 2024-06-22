e682761a391147236903b533132be46caae5d08a
Angelillo15 <contact@angelillo15.es>
2024-06-22 17:34:29 +0200

    Refactor albums.js for improved readability and maintainability

---
+++ album-data.js
@@ -0,0 +1,264 @@
+const albums = [
+  {
+    image: "assets/albums/1-nena-trampa-deluxe.jpg",
+    title: "Nena Trampa Deluxe",
+    url: "https://open.spotify.com/intl-es/album/7ntr88BWCrwZGySZQlHgBM?si=zHxZBzdyRkulxeV3QzunzA",
+    songs: [
+      {
+        name: "Jefa",
+        authors: "Cazzu",
+      },
+      {
+        name: "Nena Trampa",
+        authors: "Cazzu",
+      },
+      {
+        name: "Isla Velde",
+        authors: "Cazzu",
+      },
+      {
+        name: "Yo Yo Yo",
+        authors: "Cazzu",
+      },
+      {
+        name: "Piensame",
+        authors: "Cazzu",
+      },
+      {
+        name: "Peli-Culeo",
+        authors: "Cazzu, De La Ghetto, Randy",
+      },
+      {
+        name: "Fulete",
+        authors: "Cazzu, Brray, Luar La L, Ankhal",
+      },
+      {
+        name: "La Trampa",
+        authors: "Cazzu",
+      },
+      {
+        name: "Maléfica",
+        authors: "Cazzu, Maria Becerra",
+      },
+      {
+        name: "Los Hombres no lloran",
+        authors: "Cazzu",
+      },
+      {
+        name: "Peli-Culeo - Remix",
+        authors: "Cazzu, De La Ghetto, Randy, Ñengo Flow",
+      },
+      {
+        name: "Brinca",
+        authors: "Cazzu, Young Miko",
+      },
+      {
+        name: "Glock",
+        authors: "Cazzu, La Joaqui",
+      },
+      {
+        name: "Piénsame - Mariachi",
+        authors: "Cazzu",
+      },
+      {
+        name: "Los Hombres No Lloran - Orquesta",
+        authors: "Cazzu",
+      },
+    ],
+  },
+  {
+    image: "assets/albums/2-nena-trampa.jpg",
+    title: "Nena Trampa",
+    url: "https://open.spotify.com/intl-es/album/6hPsRT5cjxpBOgMfIupL48?si=a7dUcDxhTkWUSMjF4meRtg",
+    songs: [
+      {
+        name: "Jefa",
+        authors: "Cazzu",
+      },
+      {
+        name: "Nena Trampa",
+        authors: "Cazzu",
+      },
+      {
+        name: "Isla Velde",
+        authors: "Cazzu",
+      },
+      {
+        name: "Yo Yo Yo",
+        authors: "Cazzu",
+      },
+      {
+        name: "Piensame",
+        authors: "Cazzu",
+      },
+      {
+        name: "Peli-Culeo",
+        authors: "Cazzu, De La Ghetto, Randy",
+      },
+      {
+        name: "Fulete",
+        authors: "Cazzu, Brray, Luar La L, Ankhal",
+      },
+      {
+        name: "La Trampa",
+        authors: "Cazzu",
+      },
+      {
+        name: "Maléfica",
+        authors: "Cazzu, Maria Becerra",
+      },
+      {
+        name: "Los Hombres no lloran",
+        authors: "Cazzu",
+      },
+    ],
+  },
+  {
+    image: "assets/albums/3-bonus-trap.jpg",
+    title: "Bonus trap",
+    url: "https://open.spotify.com/intl-es/album/7IAJrYDUHZhIGXY6pY1GPX",
+    songs: [
+      {
+        name: "Bounce",
+        authors: "Cazzu",
+      },
+      {
+        name: "Esquina",
+        authors: "Cazzu, Eladio Carrion, Noriel",
+      },
+      {
+        name: "Me Prefiere a Mi",
+        authors: "Cazzu, Myke Towers",
+      }
+    ]
+  },
+  {
+    image: "assets/albums/4-una-nina-inutil.jpg",
+    title: "Una niña inutil",
+    url: "https://open.spotify.com/intl-es/album/3cOK1UKpkM2v2xtcbNFzzc?si=__Le64hvS4qYJcOxPMW8Kg",
+    songs: [
+      {
+        name: "Una niña inutil",
+        authors: "Cazzu",
+      },
+      {
+        name: "Romance de la Venganza",
+        authors: "Cazzu",
+      },
+      {
+        name: "Miedo",
+        authors: "Cazzu",
+      },
+      {
+        name: "Canción De La Mujer Astuta",
+        authors: "Cazzu",
+      },
+      {
+        name: "Conversación",	
+        authors: "Cazzu, Chita",
+      },
+      {
+        name: "Capricho",
+        authors: "Cazzu",
+      },
+      {
+        name: "Queja",
+        authors: "Cazzu, Choclock",
+      }
+    ]
+  },
+  {
+    image: "assets/albums/5-error93.jpg",
+    title: "Error 93",
+    url: "https://open.spotify.com/intl-es/album/5zbO01D6uIqC2ejQSQEN2O?si=kEIZ382MRN6-_WppPTlq6g",
+    songs: [
+      {
+        name: "Visto a las 00:00",
+        authors: "Cazzu",
+      },
+      {
+        name: "Mucha Data",
+        authors: "Cazzu",
+      },
+      {
+        name: "Rally",
+        authors: "Cazzu",
+      },
+      {
+        name: "Penas y Problemas",
+        authors: "Cazzu",
+      },
+      {
+        name: "Mentiste",
+        authors: "Cazzu",
+      },
+      {
+        name: "Al Revés",
+        authors: "Cazzu",
+      },
+      {
+        name: "Ya no quiero",
+        authors: "Cazzu",
+      },
+      {
+        name: "Fuego",
+        authors: "Cazzu",
+      },
+      {
+        name: "Nada",
+        authors: "Cazzu, Lyanno, Rauw Alejandro, Dalex",
+      },
+      {
+        name: "La Clase",
+        authors: "Cazzu, Duki, Neo Pistea, YSY A",
+      }
+    ]
+  },
+  {
+    image: "assets/albums/6-maldades.jpg",
+    title: "Maldades",
+    url: "https://open.spotify.com/intl-es/album/2qazT8MwkUsaEvDik9he9z?si=eDh72zo1RW2LEEo7HZEi-Q",
+    songs: [
+      {
+        name: "Más",
+        authors: "Cazzu",
+      },
+      {
+        name: "El Don",
+        authors: "Cazzu",
+      },
+      {
+        name: "Maldade$",
+        authors: "Cazzu",
+      },
+      {
+        name: "Killa",
+        authors: "Cazzu",
+      },
+      {
+        name: "Fantasías",
+        authors: "Cazzu",
+      },
+      {
+        name: "Trucos",
+        authors: "Cazzu",
+      },
+      {
+        name: "Trampa Bitch",
+        authors: "Cazzu",
+      },
+      {
+        name: "Kaiosama",
+        authors: "Cazzu",
+      },
+      {
+        name: "R.i.p",
+        authors: "Cazzu",
+      },
+      {
+        name: "Hello Bitche$",
+        authors: "Cazzu",
+      },
+    ]
+  },
+];
--- albums.js
+++ albums.js
@@ -5,27 +5,36 @@ const get = (album, id) => {
         <h3 class="text-center text-2xl mt-2"> ${album.title} </h3>
       </div>
       <div class="modal nav-${id}">
-        <ul class="flex flex-wrap flex-col bg-stone-900 justify-center p-2 rounded-xl gap-x-16 gap-y-4 p-4">
-          ${getList(album)}
-          <li>
-            <button class="text-3xl text-[#D080EC] hover:text-white navitem font-bold" id="close" data-toggle=".nav-${id}">
-              Cerrar
-            </button>
-          </li>
-        </ul>
+        <div>
+          <ul class="flex flex-wrap flex-col bg-stone-900 min-w-[90vw] max-w-[90vw] md:min-w-[30vw] justify-center p-2 rounded-xl gap-x-16 gap-y-4 p-4">
+            ${getList(album)}
+            <li>
+              <a href="${album.url}" target="_blank" class="flex justify-center">
+                <img class="h-24" src="assets/spotify.png">
+              </a>
+            </li>
+            <li>
+              <button class="text-3xl text-[#D080EC] hover:text-white navitem font-bold" id="close" data-toggle=".nav-${id}">
+                Cerrar
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
-  `
+  `;
 };
 
-const getList = (album)  => {
-  var sb = '';
+const getList = (album) => {
+  var sb = "";
 
   if (album.songs === null || album.songs === undefined) {
-    return "<li>Canciones no especificadas </li>"
+    return "<li>Canciones no especificadas </li>";
   }
 
   album.songs.forEach((item) => {
-    sb = sb + `
+    sb =
+      sb +
+      `
       <li class="flex flex-row">
         <img src="${album.image}" class="w-12 h-12 rounded-md mr-4">
         <div>
@@ -34,106 +43,17 @@ const getList = (album)  => {
         </div>
       </li>
     `;
-  })
+  });
 
   return sb;
-}
-
-const albums = [
-  {
-    image: 'assets/albums/1-nena-trampa-deluxe.jpg',
-    title: 'Nena Trampa Deluxe',
-    songs: [
-      {
-        name: 'Jefa',
-        authors: 'Cazzu',
-      },
-      {
-        name: 'Nena Trampa',
-        authors: 'Cazzu',
-      },
-      {
-        name: 'Isla Velde',
-        authors: 'Cazzu',
-      },
-      {
-        name: 'Yo Yo Yo',
-        authors: 'Cazzu',
-      },
-      {
-        name: 'Piensame',
-        authors: 'Cazzu',
-      },
-      {
-        name: 'Peli-Culeo',
-        authors: 'Cazzu, De La Ghetto, Randy',
-      },
-      {
-        name: 'Fulete',
-        authors: 'Cazzu, Brray, Luar La L, Ankhal',
-      },
-      {
-        name: 'La Trampa',
-        authors: 'Cazzu',
-      },
-      {
-        name: 'Maléfica',
-        authors: 'Cazzu, Maria Becerra',
-      },
-      {
-        name: 'Los Hombres no lloran',
-        authors: 'Cazzu',
-      },
-      {
-        name: 'Peli-Culeo - Remix',
-        authors: 'Cazzu, De La Ghetto, Randy, Ñengo Flow',
-      },
-      {
-        name: 'Brinca',
-        authors: 'Cazzu, Young Miko',
-      },
-      {
-        name: 'Glock',
-        authors: 'Cazzu, La Joaqui',
-      },
-      {
-        name: 'Piénsame - Mariachi',
-        authors: 'Cazzu',
-      },
-      {
-        name: 'Los Hombres No Lloran - Orquesta',
-        authors: 'Cazzu',
-      },
-    ]
-  },
-  {
-    image: 'assets/albums/2-nena-trampa.jpg',
-    title: 'Nena Trampa'
-  },
-  {
-    image: 'assets/albums/3-bonus-trap.jpg',
-    title: 'Bonus trap'
-  },
-  {
-    image: 'assets/albums/4-una-nina-inutil.jpg',
-    title: 'Una niña inutil'
-  },
-  {
-    image: 'assets/albums/5-error93.jpg',
-    title: 'Error 93'
-  },
-  {
-    image: 'assets/albums/6-maldades.jpg',
-    title: 'Maldades'
-  },
-]
+};
 
 document.addEventListener("DOMContentLoaded", function () {
-  const albumContainer = $(".album-container")
-  
+  const albumContainer = $(".album-container");
+
   albums.forEach((album) => {
     const node = document.createElement("div");
-    node.innerHTML = get(album, album.title.replaceAll(' ', '-'));
+    node.innerHTML = get(album, album.title.replaceAll(" ", "-"));
     albumContainer.appendChild(node);
-  })
+  });
 });
